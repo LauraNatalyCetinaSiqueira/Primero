bcebf83f5966e2439859b66f52a1fb026ea1ae04
laura <laura@LAPTOP-D8KAAGHM>
2022-01-17 22:56:09 -0500

    Inicio
    -clase1: creacion repo
    calse1: tipo de datos

---
+++ pruebas/src/main/java/pruebas/Operadores.java
@@ -0,0 +1,32 @@
+package pruebas;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		//Operadores matemáticos: 
+		/*
+		 +
+		 -
+		 *
+		 /
+		  	*/
+		
+		
+		System.out.println();
+		
+		
+		int edad1 = 10;
+			int edad2 = 15;
+		int res = edad1 + edad2;
+		
+		
+		
+		String nombre = "Carlos";
+		String apellido = "Lopez";	
+		String nombrecom = nombre + apellido;
+		
+		}
+			
+}
+
---
+++ pruebas/src/main/java/pruebas/TipoDeDatos.java
@@ -0,0 +1,37 @@
+package pruebas;
+
+public class TipoDeDatos {
+	
+	//argumentos o parametros que pueden venir de afuera
+
+	public static void main(String[] args) {
+		//comentarios
+		//tipos de datos
+		// Edad numerioc
+		// byte < short < int < long < float < double
+		// Para las cadenas de caracteres se deben iniciarilar con = y "" comillas dobles
+		// Ejmplo
+		
+		String nombre;
+		nombre = "Laura";
+		
+		boolean flag = true;
+		boolean otroFlag = false;
+		char letra = 'a';
+		
+		System.out.println("Lopez");
+		
+		String apellido = new String  ("Lopez");
+		String apellido2 = "Lopez";
+		
+		//Operadores
+		
+	}
+		
+		
+		
+		
+		
+
+	}
+
